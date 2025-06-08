7055dfd083febd431fd7521c8d980576cf38af83
PTNhatHa <ptnhatha.it@gmail.com>
2025-06-08 12:43:26 +0700

    [FE] Update category bar: add arrow for slider

--- frontend/src/context/CategoryContext .js
+++ frontend/src/context/CategoryContext .js
@@ -6,7 +6,8 @@ const CategoryContext = createContext()
 export const useCategories = () => useContext(CategoryContext)
 
 export const CategoryProvider = ({ children }) => {
-    const [listCategories, setListCategories] = useState([])
+    const [listCategories, setListCategories] = useState(["1111", "222", "33", "44", "55", "6", "77", "888",
+        "99", "101010", "111111111111", "121212", "1313131", "14", "155555",])
     useEffect(() => {
         const fetchData = async () => {
             const response = await ArticleServices.getCategories()
